0afe7c432d75fa0c9b718a2da2b7a94a7ea9ed70
ChongXu-dev <chongxu1018@gmail.com>
2026-04-02 17:13:38 +0800

    feat: solve LC217_contains-duplicate with HashSet

---
+++ src/hash_table/LC217_contains-duplicate.java
@@ -0,0 +1,72 @@
+//Given an integer array nums, return true if any value appears at least twice 
+//in the array, and return false if every element is distinct. 
+//
+// 
+// Example 1: 
+//
+// 
+// Input: nums = [1,2,3,1] 
+// 
+//
+// Output: true 
+//
+// Explanation: 
+//
+// The element 1 occurs at the indices 0 and 3. 
+//
+// Example 2: 
+//
+// 
+// Input: nums = [1,2,3,4] 
+// 
+//
+// Output: false 
+//
+// Explanation: 
+//
+// All elements are distinct. 
+//
+// Example 3: 
+//
+// 
+// Input: nums = [1,1,1,3,3,4,3,2,4,2] 
+// 
+//
+// Output: true 
+//
+// 
+// Constraints: 
+//
+// 
+// 1 <= nums.length <= 10⁵ 
+// -10⁹ <= nums[i] <= 10⁹ 
+// 
+//
+// Related Topics Array Hash Table Sorting 👍 13926 👎 1368
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+/**
+ * [Approach]: HashSet
+ * We use a HashSet to store the elements of nums.
+ * if the return value of the add() method is false,
+ * it means the element had alrgiteady existed in the array,
+ * indicating a duplicate.
+ *
+ * *[Complexity]:
+ * -Time Complexity: O(n), as we traverse the array only once.
+ * -Space Complexity: O(n), for storing elements in the HashSet.
+ */
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for(int num : nums ){
+            if(!set.add(num)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
