a18c06b45db015f89fb92a2d82746b91f8ed2957
ChongXu-dev <chongxu1018@gmail.com>
2026-04-11 16:03:33 +0800

    feat: solve LC121_best-time-to-buy-and-sell-stock with Greedy Algorithm

---
+++ src/greedy/LC121_best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,69 @@
+//You are given an array prices where prices[i] is the price of a given stock 
+//on the iᵗʰ day. 
+//
+// You want to maximize your profit by choosing a single day to buy one stock 
+//and choosing a different day in the future to sell that stock. 
+//
+// Return the maximum profit you can achieve from this transaction. If you 
+//cannot achieve any profit, return 0. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: prices = [7,1,5,3,6,4]
+//Output: 5
+//Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 
+//6-1 = 5.
+//Note that buying on day 2 and selling on day 1 is not allowed because you 
+//must buy before you sell.
+// 
+//
+// Example 2: 
+//
+// 
+//Input: prices = [7,6,4,3,1]
+//Output: 0
+//Explanation: In this case, no transactions are done and the max profit = 0.
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 1 <= prices.length <= 10⁵ 
+// 0 <= prices[i] <= 10⁴ 
+// 
+//
+// Related Topics Array Dynamic Programming 👍 35697 👎 1417
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+/**
+ * [Approach]:
+ * Greedy Algorithm
+ * <p>
+ * *[Complexity]:
+ * -Time Complexity: O(n)
+ * -Space Complexity: O(1)
+ */
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int minPrice = Integer.MAX_VALUE;
+
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price;
+            }
+
+            if ((price - minPrice) > maxProfit) {
+                maxProfit = price - minPrice;
+            }
+        }
+
+        return maxProfit;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
