c3a57cc7a1c7f7ed4691d1221075631efe85e75c
ChongXu-dev <chongxu1018@gmail.com>
2026-05-09 22:09:17 +0800

    feat: solve LC25_reverse-nodes-in-k-group

---
+++ src/linked_list/LC25_reverse-nodes-in-k-group.java
@@ -0,0 +1,99 @@
+//Given the head of a linked list, reverse the nodes of the list k at a time, 
+//and return the modified list. 
+//
+// k is a positive integer and is less than or equal to the length of the 
+//linked list. If the number of nodes is not a multiple of k then left-out nodes, in 
+//the end, should remain as it is. 
+//
+// You may not alter the values in the list's nodes, only nodes themselves may 
+//be changed. 
+//
+// 
+// Example 1: 
+// 
+// 
+//Input: head = [1,2,3,4,5], k = 2
+//Output: [2,1,4,3,5]
+// 
+//
+// Example 2: 
+// 
+// 
+//Input: head = [1,2,3,4,5], k = 3
+//Output: [3,2,1,4,5]
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// The number of nodes in the list is n. 
+// 1 <= k <= n <= 5000 
+// 0 <= Node.val <= 1000 
+// 
+//
+// 
+// Follow-up: Can you solve the problem in O(1) extra memory space? 
+//
+// Related Topics Linked List Recursion 👍 15705 👎 807
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null || head.next == null || k == 1) {
+            return head;
+        }
+
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+
+        ListNode prev = dummy;
+
+        while (head != null) {
+            ListNode tail = prev;
+            for (int i = 0; i < k; i++) {
+                tail = tail.next;
+                if (tail == null) {
+                    return dummy.next;
+                }
+            }
+
+            ListNode nextGroup = tail.next;
+            ListNode[] reversed = reverse(prev.next, k);
+            reversed[1].next = nextGroup;
+            prev.next = reversed[0];
+            prev = reversed[1];
+            head = reversed[1].next;
+        }
+
+        return dummy.next;
+
+    }
+
+    private ListNode[] reverse(ListNode head, int k) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        for (int i = 0; i < k; i++) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return new ListNode[]{prev, head};
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
